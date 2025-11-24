e0bc3442dc0236afbf66635d88d479584bcae5be
lguietVM <lisetuieg@gmail.com>
2025-11-24 16:56:38 +0100

    removing \n setup.sh

--- srcs/requirements/wordpress/setup.sh
+++ srcs/requirements/wordpress/setup.sh
@@ -3,8 +3,8 @@
 ## Import secrets
 MYSQL_PASSWORD=$(< /run/secrets/db_user_password_file | tr -d '\n')
 # MYSQL_ADMIN_PASSWORD=$(< /run/secrets/db_admin_password_file | tr -d '\n')
-WP_ADMIN_PASSWORD=$(< /run/secrets/wp_admin_password)
-WP_USER_PASSWORD=$(< /run/secrets/wp_user_password)
+WP_ADMIN_PASSWORD=$(< /run/secrets/wp_admin_password | tr -d '\n')
+WP_USER_PASSWORD=$(< /run/secrets/wp_user_password | tr -d '\n')
 
 
 set -e
@@ -14,6 +14,15 @@ cd /var/www/html/wordpress
 
 ## Download wp only if is not existing
 if [ ! -f wp-config.php ] && [ ! -d wp-admin ]; then
+	echo "=== Waiting DB to be ready ==="
+	DB_HOST="mariadb"
+    DB_PORT=3306
+    echo "=== Waiting for MariaDB ($DB_HOST:$DB_PORT) ==="
+    until printf "" 2>/dev/null >/dev/tcp/$DB_HOST/$DB_PORT; do
+        echo "MariaDB n'est pas prêt. Attente de 3 secondes..."
+        sleep 3
+    done
+	echo "=== ✅ MariaDB est disponible. ==="
 	echo "=== DL wp==="
 	wp core download --allow-root
 
@@ -26,12 +35,8 @@ if [ ! -f wp-config.php ] && [ ! -d wp-admin ]; then
 	--allow-root
 	echo "=== ✅ WP configurated ==="
 
-	echo "=== Waiting DB to be ready ==="
-	## Wait db to be ready
-	sleep 10
-
+	sed -i "2i \$_SERVER['HTTPS'] = 'on';" wp-config.php
 	echo "=== Instal WP ==="
-	## Wordpress Installation
 	wp core install \
 	--url="https://$DOMAIN_NAME" \
 	--title="$WP_TITLE" \
@@ -52,7 +57,8 @@ if [ ! -f wp-config.php ] && [ ! -d wp-admin ]; then
 fi
 
 echo "=== Correct permissions ==="
-chown -R www-data:www-data /var/www/html
+chown -R www-data:www-data /var/www/html/wordpress
+chmod 755 /var/www/html/wordpress
 
 echo "--- Tentative de test de la configuration PHP-FPM ---"
 # Lance le service PHP-FPM en avant-plan (PID 1) et remplace le processus du shell
